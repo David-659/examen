79b6417a1d3b8e29630a1b155e566326086c69c4
David-659 <davidvelasquez659@gmail.com>
2025-05-19 10:28:42 -0500

    backup melo

--- DavidWeb/DavidWeb/settings.py
+++ DavidWeb/DavidWeb/settings.py
@@ -123,3 +123,10 @@ STATIC_ROOT = 'static_files_server/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "hosteriaelsolecito@gmail.com"
+EMAIL_HOST_PASSWORD = "msqftugylhvzfnie"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
--- DavidWeb/examen/views.py
+++ DavidWeb/examen/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
-
+from .utils import send_email_with_attachment, compress_file_to_zip
+import os, time
+import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+# Copia de seguridad manual usando la utilidad de envío de correo con adjuntos
+
+
+def backup(request):
+    # configuración de rutas a comprimir:
+    file_to_compress = '/home/manana/Documentos/david/DavidWeb/db.sqlite3'
+    zip_archive_name = '/home/manana/Documentos/david/DavidWeb/db.sqlite3.zip'
+    compress_file_to_zip(file_to_compress, zip_archive_name)
+    print("...")
+    time.sleep(2)
+    print("Compresión correcta...!")
+    print("...")
+    
+    # envío de correo con .zip adjunto
+
+    subject = "Spa SENA - Backup"
+    body = "Copia de Seguridad de la Base de Datos del Proyecto Spa SENA"
+    to_emails = ['misena.jor@gmail.com']
+
+    # Ejemplo de un archivo adjunto (podrías leerlo de un archivo real)
+    file_path = zip_archive_name
+    if os.path.exists(zip_archive_name):
+        with open(file_path, 'rb') as f:
+            file_content = f.read()
+        attachments = [('db.sqlite3.zip', file_content, 'application/zip')]
+    else:
+        attachments = None
+
+    if send_email_with_attachment(subject, body, to_emails, attachments):
+        print("Correo electrónico enviado con éxito.")
+        return HttpResponse("Correo electrónico enviado con éxito.")
+    else:
+        print("Error al enviar el correo electrónico.")
+        return HttpResponse("Error al enviar el correo electrónico.")
